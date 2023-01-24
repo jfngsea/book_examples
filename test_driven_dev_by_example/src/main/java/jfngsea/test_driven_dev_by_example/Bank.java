0c1b2a1f078f6e5314a3d549fb0f0b22a9f6b4e0
João Martins <joaofmartins@ua.pt>
2023-01-24 19:37:05 +0000

    Test Driven Development: By Example Chapter 13

--- test_driven_dev_by_example/src/main/java/jfngsea/test_driven_dev_by_example/Bank.java
+++ test_driven_dev_by_example/src/main/java/jfngsea/test_driven_dev_by_example/Bank.java
@@ -2,6 +2,6 @@ package jfngsea.test_driven_dev_by_example;
 
 public class Bank {
     Money reduce(Expression source, String to){
-        return Money.dollar(10);
+        return source.reduce(to);
     }
 }
--- test_driven_dev_by_example/src/test/java/jfngsea/test_driven_dev_by_example/MoneyTest.java
+++ test_driven_dev_by_example/src/test/java/jfngsea/test_driven_dev_by_example/MoneyTest.java
@@ -36,4 +36,20 @@ public class MoneyTest {
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
     }
+
+    @Test
+    void testReduceSum() {
+        Expression sum= new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank= new Bank();
+        Money result= bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
+    void testReduceMoney() {
+        Bank bank= new Bank();
+        Money result= bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
 }
