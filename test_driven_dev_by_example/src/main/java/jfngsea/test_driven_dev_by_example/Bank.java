c3f7b846d393aed80dedf4b92752f150074dad5d
João Martins <joaofmartins@ua.pt>
2023-01-24 20:27:40 +0000

    Test Driven Development: By Example Chapter 14

--- test_driven_dev_by_example/src/main/java/jfngsea/test_driven_dev_by_example/Bank.java
+++ test_driven_dev_by_example/src/main/java/jfngsea/test_driven_dev_by_example/Bank.java
@@ -1,7 +1,19 @@
 package jfngsea.test_driven_dev_by_example;
 
+import java.util.Hashtable;
+
 public class Bank {
+    private final Hashtable<Pair, Integer> rates= new Hashtable<>();
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
+    }
+    int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        Integer rate= rates.get(new Pair(from, to));
+        return rate;
+    }
     Money reduce(Expression source, String to){
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 }
--- test_driven_dev_by_example/src/main/java/jfngsea/test_driven_dev_by_example/Expression.java
+++ test_driven_dev_by_example/src/main/java/jfngsea/test_driven_dev_by_example/Expression.java
@@ -1,5 +1,5 @@
 package jfngsea.test_driven_dev_by_example;
 
 public interface Expression {
-    Money reduce(String to);
+    Money reduce(Bank bank, String to);
 }
