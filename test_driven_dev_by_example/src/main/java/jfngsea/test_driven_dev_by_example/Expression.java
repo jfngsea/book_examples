06db74b6049e007c18af721da6b86e0113e4fb5f
João Martins <joaofmartins@ua.pt>
2023-01-25 03:30:54 +0000

    Test Driven Development: By Example Chapter 15

--- test_driven_dev_by_example/src/main/java/jfngsea/test_driven_dev_by_example/Expression.java
+++ test_driven_dev_by_example/src/main/java/jfngsea/test_driven_dev_by_example/Expression.java
@@ -2,4 +2,7 @@ package jfngsea.test_driven_dev_by_example;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+    Expression plus(Expression addend);
+
+    Expression times(int multiplier);
 }
