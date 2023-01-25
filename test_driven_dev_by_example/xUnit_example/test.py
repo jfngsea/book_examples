be81d1d80a845d271ee84f397e2173d6644d1ac2
João Martins <joaofmartins@ua.pt>
2023-01-25 21:04:03 +0000

    Test Driven Development: By Example Chapter 19

--- test_driven_dev_by_example/xUnit_example/test.py
+++ test_driven_dev_by_example/xUnit_example/test.py
@@ -1,27 +1,37 @@
+import abc
+
+
 class TestCase:
     def __init__(self, name) -> None:
         self.name=name
+    
+    @abc.abstractmethod
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+    
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        TestCase.__init__(self, name)
-
     def testMethod(self):
         self.wasRun= 1
+    
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp=1
 
-def TestCaseTest(TestCase):
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test= WasRun("testMethod")
     def testRunning(self):
-        test= WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
-        TestCaseTest("testRunning").run()
+        self.test.run()
+        assert(self.test.wasRun)
 
-test= WasRun("testMethod")
-print (test.wasRun)
-test.run()
-print (test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
+TestCaseTest("testSetUp").run()
