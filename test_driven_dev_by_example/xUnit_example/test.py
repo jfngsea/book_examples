00c8de56d7d17b66194fcd6dc8d2587354af25cc
João Martins <joaofmartins@ua.pt>
2023-01-26 17:52:27 +0000

    Test Driven Development: By Example Chapter 21

--- test_driven_dev_by_example/xUnit_example/test.py
+++ test_driven_dev_by_example/xUnit_example/test.py
@@ -12,10 +12,13 @@ class TestCase:
         pass
 
     def run(self):
+        result= TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
     
 
 class WasRun(TestCase):
@@ -27,6 +30,9 @@ class WasRun(TestCase):
     
     def tearDown(self):
         self.log= self.log + "tearDown "
+    
+    def testBrokenMethod(self):
+        raise Exception
 
 class TestCaseTest(TestCase):
     def setUp(self):
@@ -37,4 +43,24 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert("setUp testMethod tearDown " == self.test.log)
 
-TestCaseTest("testTemplateMethod").run()
+    def testResult(self):
+        test= WasRun("testMethod")
+        result= test.run()
+        assert("1 run, 0 failed" == result.summary())
+    
+    def testFailedResult(self):
+        test= WasRun("testBrokenMethod")
+        result= test.run()
+        assert("1 run, 1 failed", result.summary)
+
+class TestResult:
+    def __init__(self) -> None:
+        self.runCount= 0
+
+    def testStarted(self):
+        self.runCount+=1
+
+    def summary(self):
+        return f'{self.runCount} run, 0 failed'
+
+TestCaseTest("testFailedResult").run()
